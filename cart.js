7f6e7619771779dad554619395113d174b195b0d
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-04-01 07:23:13 -1000

    first commit

---
+++ cart.js
@@ -0,0 +1,106 @@
+///////////////////////////////////////////////
+///////////////////CART.JS/////////////////////
+///////////////////////////////////////////////
+/*
+    In this file, you'll be writing code to
+    calculate order totals. You'll also be 
+    creating customer objects.  
+*/
+
+
+//////////////////PROBLEM 1////////////////////
+/*  
+    Below is a cart array that has food objects
+    inside. 
+
+    Write a callback below that uses the reduce
+    array method to calculate the sum of all
+    the food. 
+*/
+
+const cart = [
+    {
+        name: 'pizza', 
+        price: 9.99
+    }, 
+    {
+        name: 'pasta', 
+        price: 8.99
+    }, 
+    {
+        name: 'salad', 
+        price: 7.99
+    }
+]
+
+//CODE HERE
+
+const summedPrice = cart.reduce((acc, item) => acc + item.price, 0)
+console.log(summedPrice)
+
+
+//////////////////PROBLEM 2////////////////////
+/*  
+    Write a function called `calcFinalPrice` that
+    can take in `cartTotal`,`couponValue`,
+    and `tax` arguments. 
+
+    Inside the function, calculate the tax 
+    on the cartTotal and add it in. Subtract
+    the value of the coupon. Return the final
+    number. 
+
+    Note: the numbers passed in for `tax` will be
+    decimals, for example: .06 for a 6% tax.
+*/
+
+const calcFinalPrice = (cartTotal, couponValue, tax) => {
+    cartTotal = (cartTotal * tax) + cartTotal;
+    cartTotal = cartTotal - couponValue;
+    return cartTotal;
+}
+console.log("The final price is: ", calcFinalPrice(10, 1, 0.06)) //should return 9.6
+
+
+
+//////////////////PROBLEM 3////////////////////
+/*  
+    In this problem, you'll create a model for 
+    a customer object as well as an example
+    object. 
+
+    Plan out a customer object for the cart page.
+    Think about the information that a 
+    restaurant would need about its customers.
+
+    In the TEXT ANSWER area below, describe the
+    properties that your customer object will have
+    and why you chose those properties.
+
+    Explain what data types each property should be
+    and why you chose those data types. 
+
+    Your object should have at least 4 properties. 
+*/
+
+/*
+The following properties are all properties that are required for a restaurant to know before they are able to seat you. 
+    1) Number of guests
+    2) Allergies
+    3) Booth or Counter
+    4) Day Preferred
+
+*/
+
+/*
+    Now, create a customer object following your own
+    guidelines.
+*/
+
+const customer = {
+    numOfGuest: 3,
+    Allergies: false,
+    boothOrCounter: "booth", 
+    dayPreferred:"Saturday"
+
+}
